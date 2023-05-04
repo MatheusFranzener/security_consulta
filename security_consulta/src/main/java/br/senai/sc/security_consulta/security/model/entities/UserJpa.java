990aadc3c4d9ae6daa33b8f784db0820cab7ab38
MatheusFranzener <92130158+MatheusFranzener@users.noreply.github.com>
2023-05-04 11:17:54 -0300

    04/05 - 11:17

--- security_consulta/src/main/java/br/senai/sc/security_consulta/security/model/entities/UserJpa.java
+++ security_consulta/src/main/java/br/senai/sc/security_consulta/security/model/entities/UserJpa.java
@@ -21,6 +21,12 @@ import java.util.List;
 @JsonDeserialize(using = UserJpaDesializer.class) // Apenas usar se der erro quando for fazer determinada permissão ( ex: cadastrar somente o usuário e não der certo )
 public class UserJpa implements UserDetails {
 
+    // Se tiver um erro tipo esse: com.fasterxml.jackson.databind.exc.InvalidDefinitionException: Cannot construct instance of `org.springframework.security.core.GrantedAuthority` (no Creators, like default constructor, exist): abstract types either need to be mapped to concrete types, have custom deserializer, or contain additional type information
+    // at [Source: (String)"{"usuario":{"id":1,"nome":"Autor","email":"autor@gmail","senha":"123"},"authorities":[{"authority":"Autor"}],"accountNonExpired":true,"accountNonLocked":true,"credentialsNonExpired":true,"enabled":true
+    // Usar o JsonDeserialize
+
+    // e se tiver um erro desse: javax.xml.bind.DatatypeConverter, usar a ultima dependencia que esta no pom.xml
+
     private Usuario usuario;
 
     private Collection<GrantedAuthority> authorities;
